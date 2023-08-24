57980811a8739d636baf1f41bab0f6c440269b93
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-24 11:01:15 -0500

    Adds Creates frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import SelectedArt from "./components/selectedArt";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Initialize state for each element of an artwork
+  const [title, setTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [picture, setPicture] = useState("");
+  const [date, setDate] = useState("");
+  const [medium, setMedium] = useState("");
+  const [rec, setRec] = useState("");
 
   return (
     <>
+      <div className="App">
+      <h1>Hello Techtonica</h1>
+      <h2>Click the button to get a random book!</h2>
+        <input id="input" type="text" required></input>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={fetchData} type="submit">
+          Get an artwork
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SelectedArt title={title} artist={artist} date={date} medium={medium} picture={picture} rec={rec} />
+    </div>
     </>
   )
 }
