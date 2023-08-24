5dbdb24c7ef0a702c9faf71b61631fc6475dbf72
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-24 10:28:06 -0500

    Adds art JSON

---
+++ server/art.js
@@ -0,0 +1,119 @@
+const BOOKS = [{
+    "id": "0",
+    "title": "The Kiss",
+    "artist": "Gustav Klimt",
+    "picture": "./Paintings/The Kiss.jpeg",
+    "date": "1907",
+    "medium": "oil and gold leaf on canvas",
+    "recommended for": "test"
+},
+{
+    "id": "1",
+    "title": "The Waltz",
+    "artist": "Camille Claudel",
+    "picture": "./Paintings/The Waltz.webp",
+    "date": "1889-1905",
+    "medium": "bronze cast",
+    "recommended for": "Rachel"
+},
+{
+    "id": "2",
+    "title": "Young Man in a Yellow Robe",
+    "artist": "Jan Lievens",
+    "picture": "./Paintings/YoungManYellowRobe.jpeg",
+    "date": "1630-1631",
+    "medium": "oil on canvas",
+    "recommended for": "Jia"
+},
+{
+    "id": "3",
+    "title": "The Last Judgment",
+    "artist": "Michelangelo",
+    "picture": "./Paintings/LastJudgement.jpeg",
+    "date": "1536-1541",
+    "medium": "fresco",
+    "recommended for": "Natalia"
+},
+{
+    "id": "4",
+    "title": "The Seven Works of Mercy",
+    "artist": "Caravaggio",
+    "picture": "./Paintings/7WorksMercy.jpeg",
+    "date": "1607",
+    "medium": "oil on canvas",
+    "recommended for": "Cristina"
+},
+{
+    "id": "5",
+    "title": "Montagne Sainte-Victoire",
+    "artist": "Paul Cezanne",
+    "picture": "./Paintings/Cezanne.jpeg",
+    "date": "1890-1895",
+    "medium": "oil on canvas",
+    "recommended for": "Cathy"
+},
+{
+    "id": "6",
+    "title": "Untitled (Valley of Death)",
+    "artist": "Zdzislaw Beksinski",
+    "picture": "./Paintings/Beksinski.webp",
+    "date": "1972",
+    "medium": "oil on canvas",
+    "recommended for": "Janet"
+},
+{
+    "id": "7",
+    "title": "The Kelpies",
+    "artist": "Andy Scott",
+    "picture": "./Paintings/Kelpies.jpeg",
+    "date": "2013",
+    "medium": "stainless steel",
+    "recommended for": "Samelia"
+},
+{
+    "id": "8",
+    "title": "Dieppe",
+    "artist": "John Duncan Fergusson",
+    "picture": "./Paintings/Dieppe.jpeg",
+    "date": "1905",
+    "medium": "oil on canvas",
+    "recommended for": "Beyza"
+},
+{
+    "id": "9",
+    "title": "Mountains at Collioure",
+    "artist": "Andre Derain",
+    "picture": "./Paintings/Mountains.png",
+    "date": "1905",
+    "medium": "oil on canvas",
+    "recommended for": "Destinee"
+},
+{
+    "id": "10",
+    "title": "Pillars of Creation",
+    "artist": "Hubble Space Telescope",
+    "picture": "./Paintings/pillars.jpeg",
+    "date": "1995",
+    "medium": "photography",
+    "recommended for": "Luke"
+},
+{
+    "id": "11",
+    "title": "The Reader",
+    "artist": "Mary Cassatt",
+    "picture": "./Paintings/Reader.jpeg",
+    "date": "1878",
+    "medium": "oil on canvas",
+    "recommended for": "Sarah"
+},
+{
+    "id": "12",
+    "title": "Mere et Enfant (Mother and Child",
+    "artist": "Pablo Picasso",
+    "picture": "./Paintings/MereEtEnfant.jpeg",
+    "date": "1902",
+    "medium": "oil on paper laid on canvas",
+    "recommended for": "Whitney-Rene"
+}
+]
+export default BOOKS;
